369006d7593786ae5a1429154fd54811e292ee3d
Karczel <92095164+Karczel@users.noreply.github.com>
2024-03-12 16:22:07 +0700

    Update test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -18,31 +18,25 @@ class TestCircle(unittest.TestCase):
         self.c2 = Circle(4)
         self.c0 = Circle(0)
 
-    # to test:
-    #1. Typical case: Test `add_area` with two circles having positive radii.
-# 2. Edge case: Test `add_area` when one of the circles has radius 0,
-#    the other has non-zero radius. (Result should have same radius.)
-#    Does `add_area` work the same regardless of which circle has radius 0?
-
     # Typical case
 
     def test_add_area(self):
         self.set_up()
         c = self.c1.get_area()
-        self.c1.add_area(self.c2)
+        self.c1 = self.c1.add_area(self.c2)
         self.assertEqual(5,self.c1.get_radius())
-        self.assertGreater(self.c1, c)
+        self.assertGreater(self.c1.get_area(), c)
 
     # Edge case
 
     def test_add_area_zero_back(self):
         self.set_up()
         c = self.c1.get_area()
-        self.c1.add_area(self.c0)
+        self.c1 = self.c1.add_area(self.c0)
         self.assertEqual(self.c1.get_area(), c)
 
     def test_add_area_zero_front(self):
         self.set_up()
         c = self.c1.get_area()
-        self.c0.add_area(self.c1)
+        self.c0 = self.c0.add_area(self.c1)
         self.assertEqual(c, self.c0.get_area())
